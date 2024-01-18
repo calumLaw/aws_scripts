ca43c79bccc9332f5d6af29c185b4b637550f3ca
Calum Lawrence <119591322+calumLaw@users.noreply.github.com>
2024-01-18 15:45:16 +0000

    Update add_widget.py

--- add_widget.py
+++ add_widget.py
@@ -2,23 +2,36 @@ import boto3
 import json
 import argparse
 
-FLOW_NAME_KEYPHRASE = "WBD-"
+FLOW_NAME_KEYPHRASE = "" # Replace with required string/keyphrase in Flow Output Name
 
 def list_filtered_outputs(search_string):
     mediaconnect_client = boto3.client('mediaconnect')
     flows_response = mediaconnect_client.list_flows()
     desired_outputs = []
+    page_count = 1
 
 
-    for flow in flows_response['Flows']:
-        flow_response = mediaconnect_client.describe_flow(FlowArn=flow['FlowArn'])
-        for output in flow_response['Flow']['Outputs']:
-            if search_string in output['Name']:
-                if FLOW_NAME_KEYPHRASE in output['Name']:
-                    desired_outputs.append(output['OutputArn'])
-                    print(f"{output['OutputArn']} added to list.")
-                else:
-                    print(f"Output does not contain keyphrase: {FLOW_NAME_KEYPHRASE}")
+    while True:
+
+        for flow in flows_response['Flows']:
+
+            flow_response = mediaconnect_client.describe_flow(FlowArn=flow['FlowArn'])
+            for output in flow_response['Flow']['Outputs']:
+                if search_string in output['Name']:
+                    if FLOW_NAME_KEYPHRASE in output['Name']:
+                        desired_outputs.append(output['OutputArn'])
+                        print(f"{output['OutputArn']} added to list.")
+                    else:
+                        print(f"Output does not contain keyphrase: {FLOW_NAME_KEYPHRASE}")
+        
+         # Check for NextToken to continue pagination
+        if 'NextToken' in flows_response:
+            flows_response = mediaconnect_client.list_flows(NextToken=flows_response['NextToken'])
+            print(f"Page {page_count} search of MediaConnect Flow Names complete. Searching next page")
+            page_count += 1
+            
+        else:
+            break  # Exit loop if no more pages
     
     
     return desired_outputs
@@ -26,7 +39,7 @@ def list_filtered_outputs(search_string):
 def create_widget(arns, search_string):
 
     metrics = [
-        [{ "expression": "SUM(METRICS())", "label": "Total", "id": "e1", "region": "eu-west-1", "period": 60, "color": "#2ca02c" }]
+        [{ "expression": "SUM(METRICS())", "label": f"{search_string} Connections", "id": "e1", "region": "eu-west-1", "period": 60, "color": "#2ca02c" }]
     ]
 
     for i, arn in enumerate(arns):
